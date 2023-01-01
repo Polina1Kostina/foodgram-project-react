464c87dc8127a253810b302eb9faf9e96d2b5e3f
unknown <kostina.polly@gmail.com>
2023-01-01 12:55:14 +0300

    test

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -71,17 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
-#DATABASES = {
-    #'default': {
-        #'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-        #'NAME': os.getenv('DB_NAME', 'postgres'),
-        #'USER': os.getenv('POSTGRES_USER', 'postgres'),
-        #'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-        #'HOST': os.getenv('DB_HOST', 'db'),
-        #'PORT': os.getenv('DB_PORT', '5432')
-    #}
-#}
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
