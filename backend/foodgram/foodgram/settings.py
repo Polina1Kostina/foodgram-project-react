059e8101762dc52483b5066640c70a5a2b04a957
unknown <kostina.polly@gmail.com>
2023-01-07 21:58:17 +0300

    test

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -127,6 +127,7 @@ DJOSER = {
         'current_user': 'api.serializers.AuthorSerializer'}}
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticatedOrReadOnly'],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
